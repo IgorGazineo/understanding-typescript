b38804d0aa889193ad5e103f6cadd0f252866ccf
Igor Gazineo <gaumgazineo@hotmail.com>
2024-03-01 00:32:48 -0300

    day 13

--- 02-basics-and-basic-types/project-setup/app.js
+++ 02-basics-and-basic-types/project-setup/app.js
@@ -4,7 +4,14 @@ function add(n1, n2) {
 function printResult(num) {
     console.log("Result: " + num);
 }
+function addAndHandle(n1, n2, cd) {
+    var result = n1 + n2;
+    cd(result);
+}
 printResult(add(5, 12));
 var combineValues;
 combineValues = add;
 console.log(combineValues(8, 8));
+addAndHandle(12, 12, function (res) {
+    console.log(res);
+});
--- 02-basics-and-basic-types/project-setup/app.ts
+++ 02-basics-and-basic-types/project-setup/app.ts
@@ -6,6 +6,11 @@ function printResult(num: number) {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cd: (num: number) => void) {
+  const result = n1 + n2;
+  cd(result);
+}
+
 printResult(add(5, 12));
 
 let combineValues: (a: number, b: number) => number;
@@ -13,3 +18,7 @@ let combineValues: (a: number, b: number) => number;
 combineValues = add;
 
 console.log(combineValues(8, 8));
+
+addAndHandle(12, 12, (res) => {
+  console.log(res);
+});
