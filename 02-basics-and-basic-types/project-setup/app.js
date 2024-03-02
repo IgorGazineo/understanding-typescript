7a99a67b61de7f395ece6b3e6092c64e44fade89
Igor Gazineo <gaumgazineo@hotmail.com>
2024-03-02 13:00:21 -0300

    day 15

--- 02-basics-and-basic-types/project-setup/app.js
+++ 02-basics-and-basic-types/project-setup/app.js
@@ -1,17 +1,12 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result: " + num);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-function addAndHandle(n1, n2, cd) {
-    var result = n1 + n2;
-    cd(result);
-}
-printResult(add(5, 12));
-var combineValues;
-combineValues = add;
-console.log(combineValues(8, 8));
-addAndHandle(12, 12, function (res) {
-    console.log(res);
-});
+var result = generateError("An error occurred!", 500);
+console.log("OUTPUT", result);
